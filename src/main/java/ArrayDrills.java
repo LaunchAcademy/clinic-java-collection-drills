dfe983a48e12f4cfe094183ace5f4c0fa25fa884
Patrick Whelan <patrickawhelan@gmail.com>
2020-03-24 14:12:02 -0400

    reverted to starting state

--- src/main/java/ArrayDrills.java
+++ src/main/java/ArrayDrills.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class ArrayDrills {
   private static List<String> teachers;
@@ -13,66 +12,33 @@ public class ArrayDrills {
     teachers = new ArrayList<String>();
 
     //read teachers from file - put it in an array list
-    Scanner scanner = new Scanner(teacherFile);
-    while(scanner.hasNextLine()) {
-      teachers.add(scanner.nextLine());
-    }
-    scanner.close();
+
 
     //list all of the teachers
-    for(String teacher : teachers) {
-      System.out.println(teacher);
-    }
+
 
     //list all of the teachers with the letter i in their name
-    for(String teacher : teachers) {
-      if(teacher.contains("i")) {
-        System.out.println(teacher);
-      }
-    }
+
 
     //output the number of teachers
-    System.out.println(teachers.size());
+
 
     //Dan is going on sabbatical. Remove him from the teachers list
-    teachers.remove("Pickett");
 
     //There's a new crew of teachers newTeachers.txt - add them to the end of the list
     //Save these peeps to a separate array. We'll need them later
-    String newPath = ArrayDrills.class.getResource("newTeachers.txt").getFile();
-    File newTeacherFile = new File(newPath);
-    Scanner newScanner = new Scanner(newTeacherFile);
 
-    List<String> newTeachers = new ArrayList<String>();
-    while(newScanner.hasNextLine()) {
-      String teacher = newScanner.nextLine();
-      teachers.add(teacher);
-      newTeachers.add(teacher);
-    }
 
     //Dan is back, add him to as backup to Amy Lynn Arrington
-    teachers.add(1, "Pickett");
 
     //what index does Kincart appear on?
-    System.out.println(teachers.indexOf("Kincart"));
 
     //the new teachers were only temporary additions. Let's remove them
-    teachers.removeAll(newTeachers);
 
     //use a traditional for loop to output the list of teachers
-    for(int i = 0; i < teachers.size(); i++) {
-      System.out.println(teachers.get(i));
-    }
 
     //find the teacher that shows up first in the alphabet
     //you can't use sort
-    String firstTeacher = teachers.get(0);
-    for(String teacher : teachers) {
-      if(teacher.compareTo(firstTeacher) < 0) {
-        firstTeacher = teacher;
-      }
-    }
-    System.out.println(firstTeacher);
 
   }
 }
--- src/main/java/HashMapDrills.java
+++ src/main/java/HashMapDrills.java
@@ -1,11 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 
 public class HashMapDrills {
 
@@ -15,36 +10,18 @@ public class HashMapDrills {
     Scanner scanner = new Scanner(teacherFile);
 
     //Create a HashMap of student names to email addresses
-    Map<String, String> contacts = new HashMap<String, String>();
-    while(scanner.hasNextLine()) {
-      String[] pair = scanner.nextLine().split(", ");
-      contacts.put(pair[0], pair[1]);
-    }
-    scanner.close();
+
 
     //list out each student's name
-    Set<String> names = contacts.keySet();
-    for(String name : names) {
-      System.out.println(name);
-    }
+
 
     //using data from the above task, output all emails on file
-    for(String name : names) {
-      System.out.println(contacts.get(name));
-    }
+
 
     //create an array list of all names that have thenorth.com in their email
-    List<String> northerners = new ArrayList<String>();
-    final String northEmail = "thenorth.com";
-    for(String name : names) {
-      if(contacts.get(name).contains(northEmail)) {
-        northerners.add(name);
-      }
-    }
+
 
     //output the northerner names
-    for(String name : northerners) {
-      System.out.println(name);
-    }
+
   }
 }
