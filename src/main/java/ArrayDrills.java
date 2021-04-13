2443ea832234696dcc9c8cadebbc354f936f0a00
KevinTranLA <kevin.tran@launchacademy.co>
2021-04-13 07:25:03 -0700

    update with final code

--- src/main/java/ArrayDrills.java
+++ src/main/java/ArrayDrills.java
@@ -1,7 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ArrayDrills {
@@ -14,29 +17,102 @@ public class ArrayDrills {
     teachers = new ArrayList<String>();
 
     //read teachers from file - put it in an array list
+    Scanner teacherScan = new Scanner(teacherFile);
+    while(teacherScan.hasNextLine()) {
+      teachers.add(teacherScan.nextLine());
+    }
+
+    teacherScan.close();
 
     //list all of the teachers
+    for(String teacher : teachers) {
+      System.out.println(teacher);
+    }
+
+    System.out.println("============");
 
     //list all of the teachers with the letter i in their name
+    for(String teacher : teachers) {
+      if(teacher.toLowerCase().contains("i")) {
+        System.out.println(teacher);
+      }
+    }
 
     //output the number of teachers
+    System.out.println("Number of teachers: " + teachers.size());
 
-    //Dan is going on sabbatical. Remove him from the teachers list
+    //"Pickett" is going on sabbatical. Remove him from the teachers list
+    teachers.remove("Pickett");
+    System.out.println("Number of teachers after removal: " + teachers.size());
 
+    System.out.println("====================");
     //There's a new crew of teachers newTeachers.txt - add them to the end of the list
     //Save these peeps to a separate array. We'll need them later
     File newTeacherFile = new File("src/main/resources/newTeachers.txt");
+    List<String> newTeachers = new ArrayList<String>();
+
+    teacherScan = new Scanner(newTeacherFile);
+    while(teacherScan.hasNextLine()) {
+      String newTeacherInput = teacherScan.nextLine();
+      teachers.add(newTeacherInput);
+      newTeachers.add(newTeacherInput);
+    }
+
+    teacherScan.close();
+
+    for(String teacher : teachers) {
+      System.out.println("old teacher list: " + teacher);
+    }
+
+    for(String newTeacher : newTeachers) {
+      System.out.println("new teacher list: " + newTeacher);
+    }
+
+    System.out.println("==================");
 
     //Dan is back, add him to as backup to Amy Lynn Arrington
+    int indexOfArrington = teachers.indexOf("Arrington");
+    if(indexOfArrington > -1) {
+      teachers.add(indexOfArrington+1, "Pickett");
+    }
 
-    //what index does Kincart appear on?
+    printTeachers(teachers);
 
-    //the new teachers were only temporary additions. Let's remove them
+    //what index does "Kincart" appear on?
+    System.out.println(teachers.indexOf("Kincart"));
+
+    //the new teachers were only temporary additions. Let's remove them from the teachers list
+//    for(String currentTeacher : newTeachers){
+//      if(teachers.contains(currentTeacher)){
+//        teachers.remove(currentTeacher);
+//      }
+//    }
+    teachers.removeAll(newTeachers);
+
+    printTeachers(teachers);
 
     //use a traditional for loop to output the list of teachers
+    for(int i = 0; i < teachers.size(); i++){
+      System.out.println(teachers.get(i));
+    }
 
     //find the teacher that shows up first in the alphabet
     //you can't use sort
+    String firstTeacher = teachers.get(0);
 
+    for(String teacher: teachers){
+        if(firstTeacher.compareTo(teacher) > 0) {
+          firstTeacher = teacher;
+        }
+
+    }
+    System.out.println(firstTeacher);
+  }
+
+  private static void printTeachers(List<String> data){
+    System.out.println("====================");
+    for(String teacher : data) {
+      System.out.println("old teacher list: " + teacher);
+    }
   }
 }
--- src/main/java/HashMapDrills.java
+++ src/main/java/HashMapDrills.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,15 +16,48 @@ public class HashMapDrills {
     File teacherFile = new File("src/main/resources/studentEmails.txt");
     Scanner scanner = new Scanner(teacherFile);
 
+    Map<String, String> studentMap = new HashMap<String, String>();
+
     //Create a HashMap of student names to email addresses
+    while(scanner.hasNextLine()) {
+      String nextLine = scanner.nextLine();
+      String[] splitData = nextLine.split(", ");
+      studentMap.put(splitData[0], splitData[1]);
+    }
+
+    scanner.close();
+    System.out.println( studentMap );
 
     //list out each student's name
+    Set<String> names = studentMap.keySet();
+    studentMap.values();
+    System.out.println(names);
 
+    System.out.println("========");
     //using data from the above task, output all emails on file
+    //for(String name : studentMap.keySet())
+    for(String name : studentMap.values()) {
+      //System.out.println(studentMap.get(name));
+      System.out.println(name);
+    }
 
+    System.out.println(studentMap.values());
     //create an array list of all names that have thenorth.com in their email
+    List<String> theNorth = new ArrayList<String>();
+    String north = "thenorth.com";
 
+    for(String email : studentMap.values()) {
+      if(email.contains(north)){
+        theNorth.add(email);
+      }
+    }
+
+    System.out.println(theNorth);
     //output the northerner names
-
+    for(Map.Entry<String, String> nameEmail : studentMap.entrySet()) {
+      if (nameEmail.getValue().contains(north)) {
+        System.out.println(nameEmail.getKey());
+      }
+    }
   }
 }
